3a61bf70c09be6f496519514cf8548b587fd5963
黄金アヒル <167955655+zolotayautka@users.noreply.github.com>
2025-12-27 06:58:24 +0900

    Add files via upload

---
+++ client/MdNotes ios/MdNotes/MdNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MdNotesApp.swift
+//  MdNotes
+//
+//  Created by 신석주 on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MdNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ client/MdNotes mac/MdNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MdNotes
+//
+//  Created by 신석주 on 12/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
